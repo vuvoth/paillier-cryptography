6959179816254b21a042600c9fdde18ff49bca79
Vu Vo <vu.voth@gmail.com>
2025-02-09 15:44:11 +0700

    encrypt and decrypt (notworking now)
    Signed-off-by: Vu Vo <vu.voth@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -20,44 +20,86 @@ pub struct Paillier {
     sk: PaillierSK
 }
 
-pub fn keygen(bits: usize) -> Paillier {
-    let p = BigNumber::safe_prime(bits);
-    let q = BigNumber::safe_prime(bits);
 
-    let n = p.clone() * q.clone();
+
+impl Paillier {
+    pub fn keygen(bits: usize) -> Paillier {
+        let p = BigNumber::safe_prime(bits);
+        let q = BigNumber::safe_prime(bits);
+
+        let n = p.clone() * q.clone();
     
-    let pk = PaillierPK {
-        g: n.clone() + BigNumber::one(),
-        n: n.clone()
-    };
+        let pk = PaillierPK {
+            g: n.clone() + BigNumber::one(),
+            n: n.clone()
+        };
 
 
-    let phi = (p.clone() - BigNumber::one()) * (q.clone() - BigNumber::one());
-    let mi = phi.invert(&n).expect("should have invert");
-    let sk = PaillierSK {
-        phi,
-        mi,
-        n
-    };
+        let phi = (p.clone() - BigNumber::one()) * (q.clone() - BigNumber::one());
 
-    Paillier {
-        pk,
-        sk
+        println!("{}", phi.to_string());
+        let mi = phi.invert(&n).expect("should have invert");
+        let sk = PaillierSK {
+            phi,
+            mi,
+            n
+        };
+
+        Paillier {
+            pk,
+            sk
+        }
+    }
+
+    pub fn encrypt(&self, m: &BigNumber) -> BigNumber {
+        if !m.gcd(&self.pk.n).is_one() {
+            panic!("message should coprime with n");
+        }
+
+        let n = self.pk.n.clone();
+        let g = self.pk.g.clone();
+        // gen random r 
+        let mut r = BigNumber::random(&n);
+        while !r.gcd(&self.pk.n).is_one() {
+            r = BigNumber::random(&n);
+        }
+
+        let n_square = n.clone() * n.clone();
+        
+        let g_power = g.modpow(m, &n_square);
+        let r_power = r.modpow(&n, &n_square);
+        
+        g_power.modmul(&r_power, &n_square)
+    }
+
+    pub fn decrypt(&self, c: &BigNumber) -> BigNumber {
+        let phi = self.sk.phi.clone();
+        let mi = self.sk.mi.clone();
+        let n = self.sk.n.clone();
+        let n_square = n.clone() * n.clone();
+        let d = c.modpow(&phi, &n_square);
+        let e = (d - BigNumber::one()) / n.clone();
+        e.modmul(&mi, &n)
     }
 }
-
-
 
 
 #[cfg(test)]
 mod tests {
-    use crate::keygen;
-
+    use crate::Paillier;
+    use unknown_order::BigNumber;
+    
     #[test]
     fn gen_key() {
-        let pk = keygen(5);
+        let paillier = Paillier::keygen(5);
 
-        println!("{:?}", pk);
+        println!("{:?}", paillier);
+        let m = BigNumber::from(2);
+        let c = paillier.encrypt(&m);
+        let d = paillier.decrypt(&c);
+        println!("{:?}", m);
+        println!("{:?}", c);
+        println!("{:?}", d);
+        assert!(m == d);
     }
 }
-
